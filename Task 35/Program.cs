73e4c4377105cf015559ae81958b5cd9bb778c36
unknown <bdn2019962@yandex.ru>
2022-08-25 20:30:54 +0400

    Web5

---
+++ Task 35/Program.cs
@@ -0,0 +1,35 @@
+﻿//Задача 35
+int[] numbers = new int[123];
+void FillArray(int[] array, int min, int max)
+{
+  for (int i = 0; i<array.Length; i++ )
+{
+    array[i] = new Random().Next(min, max);
+}
+}
+void WriteArray(int[] array){
+    for (int i = 0; i<array.Length; i++ )
+{
+    Console.Write(array[i] + " ");
+}
+}
+
+int Pos(int[] array)
+{
+    int quantity = 0;
+    for (int i = 0; i<array.Length; i++ )
+{
+     if (array[i]  > 9 && array[i]< 100)
+    {
+      quantity++;
+    }
+  }
+  return quantity;
+}
+
+FillArray(numbers, 0, 300);
+WriteArray(numbers);
+Console.WriteLine();
+
+int quantity = Pos(numbers);
+Console.WriteLine($"Количество элементов равно: {quantity}");
---
+++ Task 38/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 38
+double[] arrayRealNumbers = new double[10];
+  for (int i = 0; i < arrayRealNumbers.Length; i++ )
+  {
+    arrayRealNumbers[i] = new Random().Next(1, 10);
+    Console.Write(arrayRealNumbers[i] + " ");
+  }
+
+double maxNumber = arrayRealNumbers[0];
+double minNumber = arrayRealNumbers[0];
+
+  for (int i = 1; i < arrayRealNumbers.Length; i++)
+  {
+    if (maxNumber < arrayRealNumbers[i])
+{
+      maxNumber = arrayRealNumbers[i];
+}
+        if (minNumber > arrayRealNumbers[i])
+{
+      minNumber = arrayRealNumbers[i];
+}
+  }
+
+  double equation = maxNumber - minNumber;
+
+  Console.WriteLine($"\nразница между между максимальным ({maxNumber}) и минимальным ({minNumber}) элементами: {equation}");
